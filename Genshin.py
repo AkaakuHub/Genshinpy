b0470e3a6454011efc92cbba874aa722e1370e90
あかあく <105225777+AkaakuHub@users.noreply.github.com>
2024-04-25 15:48:16 +0900

    Update Genshin.py

--- Genshin.py
+++ Genshin.py
@@ -78,7 +78,7 @@ def main():
     write_error_log("----------start Genshin.py----------")
     # 動作ok
 
-    # get_charlist = False
+    get_charlist = False
     for _ in range(5):
         try:
             driver = webdriver.Firefox(
@@ -110,7 +110,9 @@ def main():
                 print(f"Error: {e}")
                 write_error_log(e)
                 charactor_source2 = charactor_source1
+                print("キャラクターソース2を1と同じにしました。")
             print("キャラリストを取得できました。")
+            get_charlist = True
             driver.quit()
             break
         except TimeoutError as e:
@@ -120,10 +122,10 @@ def main():
             print(f"Error: {e}")
             write_error_log(e)
             
-    # if not get_charlist:
-    #     print("キャラリストを取得できませんでした。")
-    #     write_error_log("!!could not get charactor list. Finish Genshin.py!!")
-    #     return
+    if not get_charlist:
+        print("キャラリストを取得できませんでした。")
+        write_error_log("!!could not get charactor list. Finish Genshin.py!!")
+        return
     # # つぎに、キャラのid,name,name_jaをjsonにする
     # 動作ok
     charactor_result = {}
