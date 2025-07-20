399793868b32901e1e1d45c9ff5a4fcde304e8c1
AkaakuHub <akaaku2021media01@gmail.com>
2025-07-20 12:41:52 +0900

    fix: script3

--- genshin-viewer/vite.config.ts
+++ genshin-viewer/vite.config.ts
@@ -3,15 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-import path from 'path';
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/Genshinpy/',
+  root: process.cwd(),
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
   },
 });
